7c34cf431f1522c5e8187974797f7f6c94cc8012
Guilherme Passos <guilherme2005.passos@gmail.com>
2024-09-13 19:59:59 -0400

    lab1_(Singer)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 class Singers {
 
-    // Public instance variables
     public String singerId;
     public String singerName;
     public String singerAddress;
@@ -16,7 +15,7 @@ class Singers {
         this.numAlbumsPublished = 0;
     }
 
-    // Constructor with 1 argument
+    // Constructor 1 argument
     public Singers(String singerId) {
         this.singerId = singerId;
         this.singerName = "";
@@ -25,7 +24,7 @@ class Singers {
         this.numAlbumsPublished = 0;
     }
 
-    // Constructor with 2 arguments
+    // Constructor 2 arguments
     public Singers(String singerId, String singerName) {
         this.singerId = singerId;
         this.singerName = singerName;
@@ -34,7 +33,7 @@ class Singers {
         this.numAlbumsPublished = 0;
     }
 
-    // Constructor with 3 arguments
+    // Constructor 3 arguments
     public Singers(String singerId, String singerName, String singerAddress) {
         this.singerId = singerId;
         this.singerName = singerName;
@@ -43,7 +42,7 @@ class Singers {
         this.numAlbumsPublished = 0;
     }
 
-    // Constructor with 4 arguments
+    // Constructor 4 arguments
     public Singers(String singerId, String singerName, String singerAddress, String dateOfBirth) {
         this.singerId = singerId;
         this.singerName = singerName;
@@ -52,7 +51,7 @@ class Singers {
         this.numAlbumsPublished = 0;
     }
 
-    // Constructor with 5 arguments
+    // Constructor 5 arguments
     public Singers(String singerId, String singerName, String singerAddress, String dateOfBirth, int numAlbumsPublished) {
         this.singerId = singerId;
         this.singerName = singerName;
@@ -61,12 +60,11 @@ class Singers {
         this.numAlbumsPublished = numAlbumsPublished;
     }
 
-    // Driver class to test the Singers class
     public static void main(String[] args) {
         // Create a Singer object using no-argument constructor
         Singers singer1 = new Singers();
 
-        // Display the default values
+        // Default values
         System.out.println("");
         System.out.println("Default values:");
         System.out.println("ID: " + singer1.singerId);
@@ -75,14 +73,13 @@ class Singers {
         System.out.println("Date of Birth: " + singer1.dateOfBirth);
         System.out.println("Number of Albums Published: " + singer1.numAlbumsPublished);
 
-        // Directly modify the public variables
+        // modify
         singer1.singerId = "S001";
         singer1.singerName = "John Doe";
         singer1.singerAddress = "123 Music St.";
         singer1.dateOfBirth = "1990-01-01";
         singer1.numAlbumsPublished = 5;
 
-        // Display the updated values
         System.out.println("");
         System.out.println("Updated values:");
         System.out.println("ID: " + singer1.singerId);
